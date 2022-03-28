8787d768b97b2b6a67fb4004341d3cadf11d1524
Joao Santos <aluno20383@ipt.pt>
2022-03-28 16:41:03 +0100

    versão inicial

---
+++ vet_meu/Models/consultas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vet_meu.Models
+{
+    public class consultas
+    {
+    }
+}
---
+++ vet_meu/Models/donos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vet_meu.Models
+{
+    public class Donos
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string NIF { get; set; }
+
+    }
+}
