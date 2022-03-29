7dafad1cf9cfcaff78bbbcf38dcec1299b97e76a
Joao Santos <aluno20383@ipt.pt>
2022-03-29 15:58:14 +0100

    versao 29-2-2022

--- vet_meu/Data/ApplicationDbContext.cs
+++ vet_meu/Data/ApplicationDbContext.cs
@@ -6,7 +6,10 @@ using System.Text;
 
 namespace vet_meu.Data
 {
-
+    //esta classe funciona como a base do nosso projeto
+    /// <summary>
+    /// 
+    /// </summary>
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
